b32996b9394592bda52700ec52704dbd831a3f67
Tim Kitslaar <tkitslaa@redhat.com>
2022-11-09 22:03:22 -0500

    Starting Accordion

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter";
+import "@webcomponents/webcomponentsjs/webcomponents-loader";
 
 createApp(App).mount("#app");
