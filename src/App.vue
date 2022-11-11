15f1274456553d3a042c0835d621ef81af06ca5e
Tim Kitslaar <tkitslaa@redhat.com>
2022-11-10 19:54:53 -0500

    Revert "Starting Accordion"
    This reverts commit b32996b9394592bda52700ec52704dbd831a3f67.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import "@patternfly/pfe-accordion";
 const framework: String = "Vue";
 </script>
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter";
-import "@webcomponents/webcomponentsjs/webcomponents-loader";
 
 createApp(App).mount("#app");
